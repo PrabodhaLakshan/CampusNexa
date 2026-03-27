56f005b8db6aa113748959e253f3d3bd4572d85e
WMPL02 <prabodalakshan02@gmail.com>
2026-03-27 13:43:42 +0530

    change file structer

---
+++ campusnexa-backend/src/main/java/com/campusnexa/CampusnexaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusnexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusnexaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusnexaBackendApplication.class, args);
+	}
+
+}
---
+++ campusnexa-backend/src/test/java/com/campusnexa/CampusnexaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusnexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusnexaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
